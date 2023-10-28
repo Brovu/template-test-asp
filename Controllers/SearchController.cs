2ad49f0084f06dd984553ff02d0cef94247958d4
Brovu <roy772003@gmail.com>
2023-10-28 17:47:52 +0700

    filter by age

--- Controllers/SearchController.cs
+++ Controllers/SearchController.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -10,34 +11,52 @@ namespace Template_Test.Controllers
     public class SearchController : Controller
     {
         // GET: Search
-        public ActionResult Index(string filter, string filter2)
+        public ActionResult Index(string filter, string filter2, int? from, int? to)
         {
             Template_63132843Entities2 db = new Template_63132843Entities2();
 
-            List<SinhVien> list = db.SinhViens.Where(m => m.tenSV.ToLower().Contains(filter.ToLower())).ToList();
-            List<SinhVien> list2 = db.SinhViens.Where(m => m.maSV.ToLower().Contains(filter2.ToLower())).ToList();
+            /*List<SinhVien> list = db.SinhViens.Where(m => m.tenSV.ToLower().Contains(filter.ToLower())).ToList();
+            List<SinhVien> list2 = db.SinhViens.Where(m => m.maSV.ToLower().Contains(filter2.ToLower())).ToList();*/
 
-            if (!string.IsNullOrEmpty(filter) && list.Count == 0)
-            {
-                ViewBag.Message = "Không tìm thấy kết quả theo tên!";
-            }
+            /* if (!string.IsNullOrEmpty(filter) && list.Count == 0)
+             {
+                 ViewBag.Message = "Không tìm thấy kết quả theo tên!";
+             }
 
-            if (!string.IsNullOrEmpty(filter2) && list2.Count == 0)
-            {
-                ViewBag.Message = "Không tìm thấy kết quả theo mã sinh viên!";
-            }
+             if (!string.IsNullOrEmpty(filter2) && list2.Count == 0)
+             {
+                 ViewBag.Message = "Không tìm thấy kết quả theo mã sinh viên!";
+             }
 
-            if (list2.Count > 0)
+             if (list2.Count > 0)
+             {
+                 return View(list2);
+             }
+             else if (list.Count > 0)
+             {
+                 return View(list);
+             }*/
+            /*
+                        if (!string.IsNullOrEmpty(filter) && listAge.Count == 0)
+                        {
+                            ViewBag.Message = "Không tìm thấy kết quả!";
+                        }*/
+
+            if (from.HasValue && to.HasValue)
             {
-                return View(list2);
-            }
-            else if (list.Count > 0)
-            {
-                return View(list);
+                var listAge = db.SinhViens.Where(m => m.age >= from && m.age <= to).ToList();
+
+                if (listAge.Count > 0)
+                {
+                    return View(listAge);
+                }
+                else
+                {
+                    ViewBag.Message = "Không tìm thấy kết quả.";
+                }
             }
 
             return View(db.SinhViens.ToList());
-
         }
     }
 }
